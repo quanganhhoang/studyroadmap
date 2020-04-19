5a98f7d90c3eac7170683042a69a119843737774
QA Hoang <qahoang317@gmail.com>
2020-04-19 10:31:21 -0700

    Add client code

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+// restart server whenever we make changes to this file
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+  app.use(
+    ["/api", "/auth/google"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
+};
