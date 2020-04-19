961df7d0400648ca86e02da7c34bfb5b1e4911cf
QA Hoang <qahoang317@gmail.com>
2020-04-19 12:01:02 -0700

    Apply material ui theme changes

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import Home from './Home';
 import reducers from './reducers';
 
 const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
-<Provider store={store}><App /></Provider>,
- document.getElementById("root")
+  <Provider store={store}><Home /></Provider>,
+  document.getElementById("root")
 );
